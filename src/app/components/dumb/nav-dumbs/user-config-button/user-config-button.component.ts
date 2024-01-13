1dd909f25d4ebaa6a2d77491d693affc0e979be7
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-12 21:26:08 -0300

    window error fixed

--- src/app/components/dumb/nav-dumbs/user-config-button/user-config-button.component.ts
+++ src/app/components/dumb/nav-dumbs/user-config-button/user-config-button.component.ts
@@ -6,17 +6,20 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user-config-button.component.css'
 })
 export class UserConfigButtonComponent {
-    prefersDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+    prefersDarkMode !: boolean;
 
     ngOnInit(): void {
-        if(this.prefersDarkMode){
-            document.body.classList.remove('light-theme');
-            document.body.classList.add('dark-theme');
-            document.getElementById("nav-bar__config-icon")?.setAttribute("src", "assets/config-light.png");
-        } else{
-            document.body.classList.remove('dark-theme');
-            document.body.classList.add('light-theme');
-            document.getElementById("nav-bar__config-icon")?.setAttribute("src", "assets/config.png");
+        if(typeof window !== 'undefined'){
+            this.prefersDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+            if(this.prefersDarkMode){
+                document.body.classList.remove('light-theme');
+                document.body.classList.add('dark-theme');
+                document.getElementById("nav-bar__config-icon")?.setAttribute("src", "assets/config-light.png");
+            } else{
+                document.body.classList.remove('dark-theme');
+                document.body.classList.add('light-theme');
+                document.getElementById("nav-bar__config-icon")?.setAttribute("src", "assets/config.png");
+            }
         }
     }
 }
